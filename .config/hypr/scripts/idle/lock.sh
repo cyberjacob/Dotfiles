7817bef9d9990767723f5c6850d956235829c9a5
Sam M <sam.m@saluscyber.com>
2024-10-05 12:24:13 +0100

    Idle scripts

---
+++ .config/hypr/scripts/idle/lock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+~/.config/hypr/scripts/idle/inhibitors/pulse.sh || exit 1
+
+hyprlock
---
+++ .config/hypr/scripts/idle/suspend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# inhibited?
+~/.config/hypr/scripts/idle/inhibitors/ac.sh || exit 1
+~/.config/hypr/scripts/idle/inhibitors/pulse.sh || exit 1
+
+sudo systemctl suspend
